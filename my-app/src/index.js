f835c8ba1f9080eaeb1a784afc7446926ca7c32f
Ali Seifeldin <ali.hossam.seif@gmail.com>
2020-08-22 01:04:02 -0400

    Added Firebase and it works !

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,158 +1,165 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-function Square(props) {
+// Initialize Firebase
+var firebase = require("firebase/app");
+require("firebase/auth");
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDIE2bQq5VnLks-c3sIW5O-YEQghUb_5dw",
+  authDomain: "uoftinder.firebaseapp.com",
+  databaseURL: "https://uoftinder.firebaseio.com",
+  projectId: "uoftinder",
+  storageBucket: "uoftinder.appspot.com",
+  messagingSenderId: "169113743182",
+  appId: "1:169113743182:web:4fa5174b1ef88e3452781f",
+  measurementId: "G-J686RH1BBL",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// const Welcome = ({ user, onSignOut }) => {
+//   // This is a dumb "stateless" component
+//   return (
+//     <div>
+//       Welcome <strong> {user.username} </strong>!{" "}
+//       <a href="javascript:;" onClick={onSignOut}>
+//         {" "}
+//         Sign out{" "}
+//       </a>{" "}
+//     </div>
+//   );
+// };
+
+class LoginForm extends React.Component {
+  // Using a class based component here because we're accessing DOM refs
+
+  handleSignIn(e) {
+    e.preventDefault();
+    let username = this.refs.username.value;
+    let password = this.refs.password.value;
+    this.props.onSignIn(username, password);
+  }
+
+  render() {
     return (
-        <button 
-            className="square" 
-            onClick={props.onClick}
-        >
-            {props.value}
-        </button>
-      );
-    }
-  
-  class Board extends React.Component {
-      renderSquare(i) {
-        return (
-          <Square
-            value={this.props.squares[i]}
-            onClick={() => this.props.onClick(i)}
-          />
-        );
-      }
-  
-    render() {
-      return (
-        <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
+      <form onSubmit={this.handleSignIn.bind(this)}>
+        <h3> Login </h3>{" "}
+        <input type="text" ref="username" placeholder="enter you username" />
+        <input type="password" ref="password" placeholder="enter password" />
+        <input type="submit" value="Login" />
+      </form>
+    );
   }
-  
-  class Game extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          history: [{
-            squares: Array(9).fill(null)
-          }],
-          stepNumber: 0,
-          xIsNext: true
-        };
-      }
+}
 
-    handleClick(i) {
-        const history = this.state.history.slice(0, this.state.stepNumber+ 1);
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
+class SignupForm extends React.Component {
+  // Using a class based component here because we're accessing DOM refs
 
-        if (calculateWinner(squares) || squares[i]) {
-          return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            history: history.concat([{
-                squares: squares
-            }]),
-            stepNumber: history.length,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-
-    jumpTo(step) {
-        this.setState({
-          stepNumber: step,
-          xIsNext: (step % 2) === 0,
-        });
-      }
-
-    render() {
-        const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
-
-        const moves = history.map((step, move) => {
-            console.log(history)
-            console.log(step)
-            console.log(move)
-
-            const desc = move ?
-            'Go to move #' + move :
-            'Go to game start';
-          return (
-            <li key={move}>
-              <button onClick={() => this.jumpTo(move)}>{desc}</button>
-            </li>
-          );
-        });
-        let status;
-        if (winner) {
-          status = 'Winner: ' + winner;
-        } else {
-          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
-        return (
-            <div className="game">
-            <div className="game-board">
-                <Board 
-                    squares={current.squares}
-                    onClick={(i) => this.handleClick(i)}
-                />
-            </div>
-            <div className="game-info">
-                <div>{status}</div>
-                <ol>{moves}</ol>
-            </div>
-            </div>
-        );
-    }
+  handleSignUp(e) {
+    e.preventDefault();
+    let username = this.refs.username.value;
+    let password = this.refs.password.value;
+    this.props.onSignUp(username, password);
   }
 
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
+  render() {
+    return (
+      <form onSubmit={this.handleSignUp.bind(this)}>
+        <h3> Sign Up </h3>{" "}
+        <input type="text" ref="username" placeholder="enter you username" />
+        <input type="password" ref="password" placeholder="enter password" />
+        <input type="submit" value="Signup" />
+      </form>
+    );
   }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
+}
 
-  
-  
-  
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // the initial application state
+    this.logedIn = false;
+  }
+
+  // App "actions" (functions that modify state)
+  signUp(username, password) {
+    // This is where you would call Firebase, an API etc...
+    // calling setState will re-render the entire app (efficiently!)
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(username, password)
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(error.message);
+        // ...
+      });
+  }
+
+  signIn(username, password) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(username, password)
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(error.message);
+        // ...
+      });
+  }
+
+  // signOut() {
+  //   // clear out user from state
+  //   this.setState({
+  //     user: null,
+  //   });
+  // }
+
+  render() {
+    // Here we pass relevant state to our child components
+    // as props. Note that functions are passed using `bind` to
+    // make sure we keep our scope to App
+    return (
+      <div>
+        {/* {" "}
+        {this.state.user ? (
+          <Welcome user={this.state.user} onSignOut={this.signOut.bind(this)} />
+        ) : ( */}
+        <React.Fragment>
+          <LoginForm onSignIn={this.signIn.bind(this)} />{" "}
+          <SignupForm onSignUp={this.signUp.bind(this)} />{" "}
+        </React.Fragment>
+        {/* )}{" "} */}
+      </div>
+    );
+  }
+  // render() {
+  //   // Here we pass relevant state to our child components
+  //   // as props. Note that functions are passed using `bind` to
+  //   // make sure we keep our scope to App
+  //   return (
+  //     <div>
+  //       <h1>My cool App</h1>
+  //       {
+  //         (this.state.user) ?
+  //           <Welcome
+  //            user={this.state.user}
+  //            onSignOut={this.signOut.bind(this)}
+  //           />
+  //         :
+  //           <LoginForm
+  //            onSignIn={this.signIn.bind(this)}
+  //           />
+  //       }
+  //     </div>
+  //   )
+
+  // }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
